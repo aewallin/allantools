9c89d17c1624f72e84a325d31ce7edd1254ff6b6
Anders Wallin <anders.e.e.wallin@gmail.com>
2014-02-11 00:16:51 +0200

    improve long_description of python package

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@
 from distutils.core import setup
 
 setup(name='AllanTools',
-      version='0.2',
+      version='0.21',
       description='Allan deviation and relates time/frequency statistics',
       author='Anders Wallin',
       author_email='anders.e.e.wallin@gmail.com',
       url='https://github.com/aewallin/allantools',
       license='GPLv3+',
       packages=['allantools',],
+      long_description ='Given phase or fractional frequency data this package calculates: ' \
+                        'Allan deviation, overlapping Allan deviation, modified Allan deviation, ' \
+                        'Hadamard deviation, overlapping Hadamard deviation, time deviation, ' \
+                        'total deviation, MTIE, TIE-rms'
      )
