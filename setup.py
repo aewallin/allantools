dcabe4532d990f11d417cd21aae443f80b3b82a2
Anders Wallin <anders.e.e.wallin@gmail.com>
2014-02-10 23:58:19 +0200

    add a minimal setup.py for packaging/installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AllanTools',
+      version='0.2',
+      description='Allan deviation and relates time/frequency statistics',
+      author='Anders Wallin',
+      author_email='anders.e.e.wallin@gmail.com',
+      url='https://github.com/aewallin/allantools',
+      license='GPLv3+',
+      packages=['allantools',],
+     )
