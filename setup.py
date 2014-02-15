1e7ecba1ba917d53647e8ab1212a81422c6253fc
Anders Wallin <anders.e.e.wallin@gmail.com>
2014-02-15 16:51:49 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='AllanTools',
-      version='0.22',
+      version='0.23',
       description='Allan deviation and related time/frequency statistics',
       author='Anders Wallin',
       author_email='anders.e.e.wallin@gmail.com',
