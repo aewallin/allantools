f42299c0f131ff0a1389d564368e4901802e3b6a
Danny Price <dan@thetelegraphic.com>
2014-08-19 11:30:03 -0400

    PEP8 reformatting
    Converted comments to docstrings, and (very minor) changes to variable
    names, paretheses, spacing, tabs, etc.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='AllanTools',
       url='https://github.com/aewallin/allantools',
       license='GPLv3+',
       packages=['allantools',],
+      requires=['numpy']
       long_description ='Given phase or fractional frequency data this package calculates: ' \
                         'Allan deviation, overlapping Allan deviation, modified Allan deviation, ' \
                         'Hadamard deviation, overlapping Hadamard deviation, time deviation, ' \
