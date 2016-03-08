221161cd00cf3ea299ad6ab8591b4e0490d188c4
Frédéric Meynadier <Frederic.Meynadier@obspm.fr>
2016-03-08 16:22:01 +0100

    Fixed "setup.py test" with setuptools >=18.4
    As noted on https://github.com/marshmallow-code/marshmallow/issues/300,
    
    setuptools.command.test.test_args was changed to a read-only property in
    
    https://bitbucket.org/pypa/setuptools/commits/cf565b66b855dd4df189b679206f9fb113681737
    
    ... so currently, executing python3 setup.py test fails (which means
    that the coverage command given in README.rst fails, too).
    
    This proposed change integrates pytest through the pytest-runner plugin
    as advised by http://pytest.org/latest/goodpractices.html#goodpractices

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-# import numpy
-import sys
-
-class PyTest(TestCommand):
-    """ Setup tests with py.test framework """
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
 
 setup(name='AllanTools',
       version='2016.02',
@@ -32,14 +9,14 @@ setup(name='AllanTools',
       author_email='anders.e.e.wallin@gmail.com',
       url='https://github.com/aewallin/allantools',
       license='GPLv3+',
-      packages=['allantools',],
-      requires=['numpy'],
-      #include_dirs=[numpy.get_include()],
+      packages=['allantools'],
+      requires=['numpy', 'scipy'],
+      setup_requires=["pytest-runner"],
       tests_require=['pytest'],
-      cmdclass={'test': PyTest},
-      long_description="""Given phase or fractional frequency data this package calculates:
-                        Allan deviation, overlapping Allan deviation, modified Allan deviation,
-                        Hadamard deviation, overlapping Hadamard deviation, time deviation,
-                        total deviation, MTIE, TIE-RMS. Synthetic noise data generators are also included."""
-     )
-
+      long_description=(
+          "Given phase or fractional frequency data this package calculates: "
+          "Allan deviation, overlapping Allan deviation, modified Allan "
+          "deviation, Hadamard deviation, overlapping Hadamard deviation, "
+          "time deviation, itotal deviation, MTIE, TIE-RMS. Synthetic noise "
+          "data generators are also included.")
+      )
