c2079dbd5f113bd4f2e7ba003c619e612e092ac4
Anders Wallin <anders.e.e.wallin@gmail.com>
2016-03-31 17:41:01 +0300

    4spaces, not tabs

--- tests/pink_frequency/test_pink.py
+++ tests/pink_frequency/test_pink.py
@@ -17,34 +17,34 @@ import testutils
 import os
 
 def test_pink():
-	# hack to run script from its own directory
-	abspath = os.path.abspath(__file__)
-	dname = os.path.dirname(abspath)
-	os.chdir(dname)
+    # hack to run script from its own directory
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
 
-	data_file = 'pink_frequency.txt'
-	adev_result = 'adev.txt'
-	oadev_result = 'oadev.txt'
-	mdev_result = 'mdev.txt'
-	tdev_result = 'tdev.txt'
-	hdev_result = 'hdev.txt'
-	ohdev_result = 'ohdev.txt'
-	totdev_result = 'totdev_alpha0.txt'
-	mtie_result = 'mtie.txt'
-	tierms_result = 'tierms.txt'
-	#verbose = 1
-	
-	tolerance = 1e-4
-	rate = 1/float(42.0) # stable32 runs were done with this data-interval
-	testutils.test( allan.adev, data_file, rate, adev_result ,frequency=True, verbose=0, tolerance=tolerance)
-	testutils.test( allan.oadev, data_file, rate, oadev_result,frequency=True, verbose=0, tolerance=tolerance )
-	testutils.test( allan.mdev, data_file, rate, mdev_result,frequency=True, verbose=0, tolerance=tolerance )
-	testutils.test( allan.tdev, data_file, rate, tdev_result,frequency=True, verbose=0, tolerance=tolerance )
-	testutils.test( allan.hdev, data_file, rate, hdev_result,frequency=True, verbose=0, tolerance=tolerance )
-	testutils.test( allan.ohdev, data_file, rate, ohdev_result,frequency=True, verbose=0, tolerance=tolerance )
-	testutils.test( allan.totdev, data_file, rate, totdev_result,frequency=True, verbose=0, tolerance=tolerance )
+    data_file = 'pink_frequency.txt'
+    adev_result = 'adev.txt'
+    oadev_result = 'oadev.txt'
+    mdev_result = 'mdev.txt'
+    tdev_result = 'tdev.txt'
+    hdev_result = 'hdev.txt'
+    ohdev_result = 'ohdev.txt'
+    totdev_result = 'totdev_alpha0.txt'
+    mtie_result = 'mtie.txt'
+    tierms_result = 'tierms.txt'
+    #verbose = 1
+    
+    tolerance = 1e-4
+    rate = 1/float(42.0) # stable32 runs were done with this data-interval
+    testutils.test( allan.adev, data_file, rate, adev_result ,frequency=True, verbose=0, tolerance=tolerance)
+    testutils.test( allan.oadev, data_file, rate, oadev_result,frequency=True, verbose=0, tolerance=tolerance )
+    testutils.test( allan.mdev, data_file, rate, mdev_result,frequency=True, verbose=0, tolerance=tolerance )
+    testutils.test( allan.tdev, data_file, rate, tdev_result,frequency=True, verbose=0, tolerance=tolerance )
+    testutils.test( allan.hdev, data_file, rate, hdev_result,frequency=True, verbose=0, tolerance=tolerance )
+    testutils.test( allan.ohdev, data_file, rate, ohdev_result,frequency=True, verbose=0, tolerance=tolerance )
+    testutils.test( allan.totdev, data_file, rate, totdev_result,frequency=True, verbose=0, tolerance=tolerance )
 
 if __name__ == "__main__":
-	test_pink()
+    test_pink()
 
 
