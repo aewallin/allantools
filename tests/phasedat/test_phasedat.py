90a3faeab77e5ddb323670dc9920f6fcd5a56d7b
Anders Wallin <anders.e.e.wallin@gmail.com>
2016-03-31 15:20:19 +0300

    pytest style test-class

--- tests/phasedat/test_phasedat.py
+++ tests/phasedat/test_phasedat.py
@@ -8,6 +8,8 @@
 
 import math
 import sys
+import pytest
+
 sys.path.append("..")
 sys.path.append("../..") # hack to import from parent directory
 # remove if you have allantools installed in your python path
@@ -17,57 +19,52 @@ import testutils
 
 import os
 
+data_file = 'PHASE.DAT'
+tolerance = 1e-4
+verbose = 1
 
-def test_phasedat():
+def change_to_test_dir():
     # hack to run script from its own directory
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
-    print(dname)
 
-    data_file = 'PHASE.DAT'
-    
-    theo1_result = 'phase_dat_theo1_alpha0.txt' # should have no bias-correction?
-    
-    htotdev_result = 'phase_dat_htotdev_octave.txt'
-    
-    ttotdev_result = 'phase_dat_ttotdev_octave.txt'
-        
-    mtotdev_result = 'phase_dat_mtotdev_octave.txt'
+class TestPhaseDat():
+    def test_phasedat_adev(self):
+        self.generic_test( result= 'phase_dat_adev.txt' , fct= allan.adev )
+    def test_phasedat_oadev(self):
+        self.generic_test( result='phase_dat_oadev.txt' , fct= allan.oadev )
+    def test_phasedat_mdev(self):
+        self.generic_test( result='phase_dat_mdev.txt' , fct= allan.mdev )
+    def test_phasedat_tdev(self):
+        self.generic_test( result='phase_dat_tdev.txt' , fct= allan.tdev )
+    def test_phasedat_hdev(self):
+        self.generic_test( result='phase_dat_hdev.txt' , fct= allan.hdev )
+    def test_phasedat_ohdev(self):
+        self.generic_test( result='phase_dat_ohdev.txt' , fct= allan.ohdev )
+    def test_phasedat_totdev(self):
+        self.generic_test( result='phase_dat_totdev.txt' , fct= allan.totdev )
+    def test_phasedat_htotdev(self):
+        # this test fails because bias-correction is not implemented
+        self.generic_test( result='phase_dat_htotdev_octave.txt' , fct= allan.htotdev )
+    def test_phasedat_mtotdev(self):
+        self.generic_test( result='phase_dat_mtotdev_octave.txt' , fct= allan.mtotdev )
+    def test_phasedat_ttotdev(self):
+        self.generic_test( result='phase_dat_ttotdev_octave.txt' , fct= allan.ttotdev )
+    def test_phasedat_theo1_alpha0(self):
+        self.generic_test( result='phase_dat_theo1_alpha0_decade.txt' , fct= allan.theo1 )
 
-    adev_result = 'phase_dat_adev.txt'
-    oadev_result = 'phase_dat_oadev.txt'
-    mdev_result = 'phase_dat_mdev.txt'
-    tdev_result = 'phase_dat_tdev.txt'
-    hdev_result = 'phase_dat_hdev.txt'
-    ohdev_result = 'phase_dat_ohdev.txt'
-    totdev_result = 'phase_dat_totdev.txt'
-    mtie_result = 'phase_dat_mtie.txt'
-    tierms_result = 'phase_dat_tierms.txt'
-    verbose = 1
+    def test_phasedat_mtie(self):
+        self.generic_test( result='phase_dat_mtie.txt' , fct= allan.mtie )
+    def test_phasedat_tierms(self):
+        self.generic_test( result='phase_dat_tierms.txt' , fct= allan.tierms )
     
-    tolerance = 1e-4
-    
-    testutils.test_row_by_row( allan.theo1, data_file, 1.0, theo1_result , verbose=verbose, tolerance=tolerance)
-    
-    testutils.test_row_by_row( allan.htotdev, data_file, 1.0, htotdev_result , verbose=verbose, tolerance=tolerance)
-    
-    testutils.test_row_by_row( allan.ttotdev, data_file, 1.0, ttotdev_result , verbose=verbose, tolerance=tolerance)
-
-    testutils.test_row_by_row( allan.mtotdev, data_file, 1.0, mtotdev_result , verbose=verbose, tolerance=tolerance)
-
-    testutils.test( allan.adev, data_file, 1.0, adev_result , verbose=verbose, tolerance=tolerance)
-    testutils.test( allan.oadev, data_file, 1.0, oadev_result, verbose=verbose, tolerance=tolerance )
-    testutils.test( allan.mdev, data_file, 1.0, mdev_result, verbose=verbose, tolerance=tolerance )
-    testutils.test( allan.tdev, data_file, 1.0, tdev_result, verbose=verbose, tolerance=tolerance )
-    testutils.test( allan.hdev, data_file, 1.0, hdev_result, verbose=verbose, tolerance=tolerance )
-    testutils.test( allan.ohdev, data_file, 1.0, ohdev_result, verbose=verbose, tolerance=tolerance )
-    testutils.test( allan.totdev, data_file, 1.0, totdev_result, verbose=verbose, tolerance=tolerance )
-    testutils.test( allan.mtie, data_file, 1.0, mtie_result, verbose=verbose, tolerance=tolerance )
-    testutils.test( allan.tierms, data_file, 1.0, tierms_result, verbose=verbose, tolerance=tolerance )
-    
+    def generic_test(self, datafile = data_file, result="", fct=None):
+        change_to_test_dir()
+        testutils.test( fct, datafile, 1.0, result , verbose=verbose, tolerance=tolerance)
 
 if __name__ == "__main__":
-    test_phasedat()
-
+    t = TestPhaseDat()
+    t.test_phasedat_theo1_alpha0()
+    #pytest.main()
 
