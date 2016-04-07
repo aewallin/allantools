b880be3ebc498bfbb8df55b5da5d1e156464976c
Anders Wallin <anders.e.e.wallin@gmail.com>
2016-04-07 07:51:41 +0300

    add test for noise.py - before this it had almost zero coverage

---
+++ tests/test_noise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append("..")
+
+from allantools import noise
+import numpy
+import pytest
+
+
+def test_noise():
+
+    N = 500
+    rate = 1.0
+    w = noise.white(N)
+    b = noise.brown(N)
+    v = noise.violet(N)
+    p = noise.pink(N)
+    
+    assert len(w) == N
+    assert len(b) == N
+    assert len(v) == N-1 # why?
+    assert len(p) == N
+    
+if __name__ == "__main__":
+    test_noise()
