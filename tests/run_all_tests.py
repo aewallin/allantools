e3d7e5e3d5c483c91e4be8b16c85cc2761dce74c
Anders Wallin <anders.e.e.wallin@gmail.com>
2016-04-08 20:05:30 +0300

    move some tests to functional_tests/ cleanup the script that runs all tests.

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -16,35 +16,15 @@ import time
 import sys
 import pytest
 
-"""
-from nbs14 import nbs14_test
-from phasedat import phase_dat_test
-from pink_frequency import pink
-from Cs5071A import Cs5071A_test_decade
-from Keysight53230A_ti_noise_floor import TIC_test
-from ocxo import ocxo_test
-from gps import gps_test
-import test_ns
-"""
-
 if __name__ == "__main__":
+    print("running all AllanTools tests. May take up to 200 seconds.")
     start = time.clock()
     pytest.main()
-    """    
-    ocxo_test.run()           # high-stability OCXO timebase on HP instrument
-    TIC_test.run()            # 53230A counter noise floor dataset
-    nbs14_test.run()          # NBS14 test data (10-point and 1000-point) with published deviations
-    phase_dat_test.run()      # phase.dat from Stable32
-    pink.run()                # synthetic pink frequency noise
-    Cs5071A_test_decade.run() # HP 5071A Cs-clock measured against H-maser
-    gps_test.run()
-    test_ns.run()             # sanity-checks for tau values
-    
-    """
     end = time.clock()
     print("-------------------------")
     print("All tests done in %2.3f s" % (end-start)) 
     # 2014-08-31 running time without MTIE on laptop with i7-3537U CPU @ 2.00GHz
     # All tests done in 19.841 s
     # 2015-05-05 17.373 s
+    # 2016-04-08: 80 tests in 174 s
     print("if we came this far without assertions or errors all is OK!")
