da94b08b956ba2410fa235aa211947ee2e2c8776
A W <anders.wallin@vtt.fi>
2018-01-12 16:17:20 +0200

    minor format edits

--- tests/test_noise_kasdin.py
+++ tests/test_noise_kasdin.py
@@ -13,43 +13,43 @@ import numpy as np
 def noisegen():
     return at.Noise()
 
-@pytest.mark.parametrize("n", range(2,20))
+@pytest.mark.parametrize("n", range(2, 20))
 def test_timeseries_length(noisegen, n):
     """
         check that the time-series is of correct length
     """
-    nr = pow(2,n)
+    nr = pow(2, n)
     noisegen.set_input(nr=nr)
     noisegen.generateNoise()
-    print( nr )
-    print( len( noisegen.time_series ) )
-    assert( len( noisegen.time_series ) == nr )
+    print(nr)
+    print(len(noisegen.time_series))
+    assert len(noisegen.time_series) == nr
 
 # failing cases
 # (b, tau, qd)
 # these seem problematic, even with nr=pow(2,16) and N_averages=30
-failing = [ (-4, 1, 6e-09),
-            (-3, 1, 6e-09),
-            (-4, 1, 5e-10),
-            (-3, 1, 5e-10),
-            (-4, 1, 3e-15),
-            (-3, 1, 3e-15) ]
+failing = [(-4, 1, 6e-09),
+           (-3, 1, 6e-09),
+           (-4, 1, 5e-10),
+           (-3, 1, 5e-10),
+           (-4, 1, 3e-15),
+           (-3, 1, 3e-15),]
 @pytest.mark.xfail
 @pytest.mark.parametrize("params", failing)
 def test_adev_average_failing(noisegen, params):
     test_adev_average(noisegen, params[0], params[1], params[2])
  
-@pytest.mark.parametrize("b",[0, -1, -2, ])
-@pytest.mark.parametrize("tau",[1,2,3,4,5, 20, 30])
-@pytest.mark.parametrize("qd",[3e-15, 5e-10, 6e-9, ]) # 7e-6 2e-20
-def test_adev_average(noisegen,b,tau,qd, nr=pow(2,16), N_averages=30, rtol=1e-1):
+@pytest.mark.parametrize("b", [0, -1, -2, ])
+@pytest.mark.parametrize("tau", [1, 2, 3, 4, 5, 20, 30])
+@pytest.mark.parametrize("qd", [3e-15, 5e-10, 6e-9, ]) # 7e-6 2e-20
+def test_adev_average(noisegen, b, tau, qd, nr=pow(2, 16), N_averages=30, rtol=1e-1):
     """
         check that time-series has the ADEV that we expect
         generate many time-series (N_averages)
         compute average adev from the multiple time-series
     """
-    noisegen.set_input(nr=nr, qd=qd , b=b)
-    adevs=[]
+    noisegen.set_input(nr=nr, qd=qd, b=b)
+    adevs = []
     for n in range(N_averages):
         noisegen.generateNoise()
         (taus,devs,errs,ns)=at.adev( noisegen.time_series, taus=[tau], rate=1.0 )
