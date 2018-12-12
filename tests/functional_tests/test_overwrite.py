4f1b06b0d58fb349de47e81dfa95eb9232e86a29
Frédéric Meynadier <frederic.meynadier@bipm.org>
2018-12-12 10:22:10 +0000

    Bugfix for issue #76

---
+++ tests/functional_tests/test_overwrite.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append("..")
+
+import allantools as at
+import numpy
+import pytest
+
+
+def test_overwrite():
+    """ Tests if data is overwritten as pointed out in issue
+    https://github.com/aewallin/allantools/issues/76
+    """
+    x1 = at.noise.white(num_points=1024)
+    x2 = at.noise.pink(N=1024)
+    ds1 = at.Dataset(x1)
+    ds2 = at.Dataset(x2)
+    r1 = ds1.compute('oadev')
+    r2 = ds2.compute('oadev')
+    # If both are identical, something is wrong...
+    assert(not numpy.allclose(r1['stat'], r2['stat']))
+
+if __name__ == "__main__":
+    test_overwrite()
