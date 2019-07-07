40c2df9f6ac8e98f6cb2cfe8d0702ea06053f638
Anders Wallin <anders.e.e.wallin@gmail.com>
2019-07-07 13:02:49 +0300

    remove unused code

--- allantools/realtime.py
+++ allantools/realtime.py
@@ -110,12 +110,6 @@ class oadev_realtime(dev_realtime):
     def add_af(self):
         self.S = numpy.append(self.S, 0)
 
-    def __str__(self):
-        msg = "n_pts: %d " % len(self.x)
-        for idx, af in enumerate(self.afs):
-            msg += "ADEV(%d)=%f " %(af, self.dev[idx])
-        return msg
-
 class ohdev_realtime(dev_realtime):
     """ Overlapping Hadamard deviation in real-time from a stream of phase/frequency samples.
 
@@ -146,13 +140,6 @@ class ohdev_realtime(dev_realtime):
         S_new = pow(self.x[i] - 3*self.x[i-af] + 3*self.x[i-2*af] - self.x[i-3*af], 2)
         self.S[idx] = self.S[idx] + S_new
         self.dev[idx] = numpy.sqrt((1.0/(6.0*pow(af*self.tau0, 2)*(i+1.0-3*af))) * self.S[idx])
-
-    def __str__(self):
-        msg = "n_pts: %d " % len(self.x)
-        for idx, af in enumerate(self.afs):
-            msg += "OHDEV(%d)=%f " %(af, self.dev[idx])
-        return msg
-
 
 class tdev_realtime(dev_realtime):
     """ Time deviation and Modified Allan deviation in real-time from a stream of phase/frequency samples.
@@ -216,54 +203,5 @@ class tdev_realtime(dev_realtime):
         for idx, af in enumerate(self.afs):
             mdev[idx] = mdev[idx]*numpy.sqrt(3)/(af*self.tau0)
         return mdev
-
-    def __str__(self):
-        msg = "n_pts: %d " % len(self.x)
-        for idx, af in enumerate(self.afs):
-            msg += "TDEV(%d)=%f " %(af, self.dev[idx])
-        return msg
-
-if __name__ == "__main__":
-    nbs14_phase = [0.00000, 103.11111, 123.22222, 157.33333, 166.44444, 48.55555, -96.33333, -2.22222, 111.88889, 0.00000]
-    nbs14_f = [892.0, 809.0, 823.0, 798.0, 671.0, 644.0, 883.0, 903.0, 677.0]
-
-    nbs14_devs = [(91.22945, 115.8082),  # 0, ADEV(tau=1,tau=2)
-                  (91.22945, 85.95287), # 1, OADEV
-                  (91.22945, 74.78849),  # 2, MDEV
-                  #(91.22945,98.31100),  # TOTDEV, http://www.ieee-uffc.org/frequency-control/learning-riley.asp
-                  (91.22945, 93.90379), # 3, TOTDEV, http://tf.nist.gov/general/pdf/2220.pdf page 107
-                  (70.80608, 116.7980),  # 4, HDEV
-                  (52.67135, 86.35831),  # 5, TDEV
-                  (70.80607, 85.61487), # 6, OHDEV
-                  #(75.50203, 75.83606),  # 7, MTOTDEV (published table, WFM bias correction)
-                  (6.4509e+01, 6.4794e+01), # MTOTDEV Stable32 v1.60, no bias-correction
-                  #(43.59112, 87.56794 ), # 8, TTOTDEV (published table, WFM bias correction)
-                  (3.7244e+01, 7.4818e+01), # TTOTDEV Stable32 v1.60, no bias-correction
-                  (70.80607, 91.16396), # 9 HTOTDEV (published table)
-                  (45.704, 81.470)] # 10 HTOTDEV Stable32 (not sure what these are!??)
-                  # (100.9770, 102.6039)  # Standard Deviation (sample, not population)
-    oadev_rt = oadev_realtime(afs=[1, 2], tau0=1.0)
-    for x in nbs14_phase:
-        oadev_rt.add_phase(x)
-    print("OADEV rt ", oadev_rt.dev[0], " == OADEV ", nbs14_devs[1][0])
-    print("OADEV rt ", oadev_rt.dev[1], " == OADEV ", nbs14_devs[1][1])
-    assert(numpy.isclose(oadev_rt.dev[0], nbs14_devs[1][0]))
-    assert(numpy.isclose(oadev_rt.dev[1], nbs14_devs[1][1]))
-
-    ohdev_rt = ohdev_realtime(afs=[1, 2], tau0=1.0)
-    for x in nbs14_phase:
-        ohdev_rt.add_phase(x)
-    print("OHDEV rt ", ohdev_rt.dev[0], " == OADEV ", nbs14_devs[6][0])
-    print("OHDEV rt ", ohdev_rt.dev[1], " == OADEV ", nbs14_devs[6][1])
-    assert(numpy.isclose(ohdev_rt.dev[0], nbs14_devs[6][0]))
-    assert(numpy.isclose(ohdev_rt.dev[1], nbs14_devs[6][1]))
-
-    tdev_rt = tdev_realtime(afs=[1, 2], tau0=1.0)
-    for x in nbs14_phase:
-        tdev_rt.add_phase(x)
-    print("mDEV rt ", tdev_rt.mdev()[0], " == mDEV ", nbs14_devs[2][0])
-    print("mDEV rt ", tdev_rt.mdev()[1], " == mDEV ", nbs14_devs[2][1])
-    assert(numpy.isclose(tdev_rt.mdev()[0], nbs14_devs[2][0]))
-    assert(numpy.isclose(tdev_rt.mdev()[1], nbs14_devs[2][1]))
 
 # end of file realtime.py
