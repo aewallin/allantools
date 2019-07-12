9b0809156cacf9a824eb8ff190d76ba24ab6c2f8
Anders Wallin <anders.e.e.wallin@gmail.com>
2019-07-12 19:16:12 +0300

    apply fix for #82 by @AntonPrygrodskyi

--- allantools/allantools.py
+++ allantools/allantools.py
@@ -1133,6 +1133,75 @@ def mtie(data, rate=1.0, data_type="phase", taus=None):
     ns = np.zeros_like(taus_used)
 
     for idx, mj in enumerate(m):
+        try:
+            # the older algorithm uses a lot of memory
+            # but can be used for short datasets.
+            rw = mtie_rolling_window(phase, int(mj + 1))
+            win_max = np.max(rw, axis=1)
+            win_min = np.min(rw, axis=1)
+            tie = win_max - win_min
+            dev = np.max(tie)
+        except:
+            if int(mj + 1) < 1:
+                raise ValueError("`window` must be at least 1.")
+            if int(mj + 1) > phase.shape[-1]:
+                raise ValueError("`window` is too long.")
+
+            mj = int(mj)
+            currMax = np.max(phase[0:mj])
+            currMin = np.min(phase[0:mj])
+            dev = currMax - currMin
+            for winStartIdx in range(1, int(phase.shape[0] - mj)):
+                winEndIdx = mj + winStartIdx
+                if currMax == phase[winStartIdx - 1]:
+                    currMax = np.max(phase[winStartIdx:winEndIdx])
+                elif currMax < phase[winEndIdx]:
+                    currMax = phase[winEndIdx]
+
+                if currMin == phase[winStartIdx - 1]:
+                    currMin = np.min(phase[winStartIdx:winEndIdx])
+                elif currMin > phase[winEndIdx]:
+                    currMin = phase[winEndIdx]
+
+                if dev < currMax - currMin:
+                    dev = currMax - currMin
+
+    ncount = phase.shape[0] - mj
+    devs[idx] = dev
+    deverrs[idx] = dev / np.sqrt(ncount)
+    ns[idx] = ncount
+
+return remove_small_ns(taus_used, devs, deverrs, ns)`
+def mtie(data, rate=1.0, data_type="phase", taus=None):
+    """ Maximum Time Interval Error.
+
+    Parameters
+    ----------
+    data: np.array
+        Input data. Provide either phase or frequency (fractional,
+        adimensional).
+    rate: float
+        The sampling rate for data, in Hz. Defaults to 1.0
+    data_type: {'phase', 'freq'}
+        Data type, i.e. phase or frequency. Defaults to "phase".
+    taus: np.array
+        Array of tau values, in seconds, for which to compute statistic.
+        Optionally set taus=["all"|"octave"|"decade"] for automatic
+        tau-list generation.
+
+    Notes
+    -----
+    this seems to correspond to Stable32 setting "Fast(u)"
+    Stable32 also has "Decade" and "Octave" modes where the
+    dataset is extended somehow?
+    """
+    phase = input_to_phase(data, rate, data_type)
+    (phase, m, taus_used) = tau_generator(phase, rate, taus)
+    devs = np.zeros_like(taus_used)
+    deverrs = np.zeros_like(taus_used)
+    ns = np.zeros_like(taus_used)
+
+    for idx, mj in enumerate(m):
         rw = mtie_rolling_window(phase, int(mj + 1))
         win_max = np.max(rw, axis=1)
         win_min = np.min(rw, axis=1)
