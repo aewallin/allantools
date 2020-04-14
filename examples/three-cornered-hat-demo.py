3a8b10ddcc1ef8e49bb61d9975ac78b67e04343d
Anders Wallin <anders.e.e.wallin@gmail.com>
2020-04-14 17:20:27 +0300

    tweak the example

--- examples/three-cornered-hat-demo.py
+++ examples/three-cornered-hat-demo.py
@@ -28,40 +28,45 @@ import allantools
 from allantools import noise
 
 def plotallan_phase(plt,y,rate,taus, style, label):
-    (t2, ad, ade,adn) = allantools.mdev(y,data_type='phase',rate=rate,taus=taus)
-    plt.loglog(t2, ad, style, label=label)
+    (t2, ad, ade,adn) = allantools.oadev(y,data_type='phase',rate=rate,taus=taus)
+    plt.loglog(t2, ad,  style, fillstyle='none', label=label,)
 
 # plot a line with the slope alpha
-def plotline(plt, alpha, taus,style):
-    y = [ pow(tt,alpha) for tt in taus]
-    plt.loglog(taus,y,style)
+def plotline(plt, alpha, amplitude, taus, style, labeltext):
+    y = [ amplitude*pow(tt,alpha) for tt in taus]
+    plt.loglog(taus,y,style, label=labeltext)
     
 if __name__ == "__main__":
     print("allatools three-cornered-hat demo")
     # we test ADEV etc. by calculations on synthetic data
     # with known slopes of ADEV
 
-    t = [xx for xx in numpy.logspace( 0 ,4,50)] # tau values from 1 to 1000
+    t = numpy.logspace( 0 ,4,50) # tau values from 1 to 1000
     plt.subplot(111, xscale="log", yscale="log")
 
     N=10000
     rate = 1.0
     # white phase noise => 1/tau ADEV
-    d = numpy.random.randn(4*N)
-    phaseA = d[0:N] # numpy.random.randn(N) #pink(N)
-    phaseA = [1*x for x in phaseA]
-    phaseB = d[N:2*N] #numpy.random.randn(N) #noise.pink(N)
-    phaseB = [5*x for x in phaseB]
-    phaseC = d[2*N:3*N] #numpy.random.randn(N) #noise.pink(N)
-    phaseC = [5*x for x in phaseC]
-
-    phaseAB = [a-b for (a,b) in zip(phaseA,phaseB)]
-    phaseBC = [b-c for (b,c) in zip(phaseB,phaseC)]
-    phaseCA = [c-a for (c,a) in zip(phaseC,phaseA)]
-
-    (taus,devA,err_a,ns_ab) = allantools.three_cornered_hat_phase(phaseAB,phaseBC,phaseCA,rate,t, allantools.mdev)
-    print("TCH devA")
     
+    ampl_A = 1.0 
+    phaseA = ampl_A*numpy.random.randn(N)
+    ampl_B = 5.0
+    phaseB = ampl_B*numpy.random.randn(N)
+    ampl_C = 10.0
+    phaseC = ampl_C*numpy.random.randn(N)
+
+    # measurements clk_I - clk_J
+    phaseAB = phaseA - phaseB # [a-b for (a,b) in zip(phaseA,phaseB)]
+    phaseBC = phaseB - phaseC # [b-c for (b,c) in zip(phaseB,phaseC)]
+    phaseCA = phaseC - phaseA # [c-a for (c,a) in zip(phaseC,phaseA)]
+
+
+    # theoretical ADEVs
+    plotline(plt, -1.0, numpy.sqrt(3)*ampl_A, t, 'r--','A model')
+    plotline(plt, -1.0, numpy.sqrt(3)*ampl_B, t, 'g--','B model')
+    plotline(plt, -1.0, numpy.sqrt(3)*ampl_C, t, 'b--','C model')
+
+    # simulated clocks
     plotallan_phase(plt, phaseA, 1, t, 'ro', 'true A phase')
     print("phaseA")
     plotallan_phase(plt, phaseB, 1, t, 'go', 'true B phase')
@@ -69,6 +74,7 @@ if __name__ == "__main__":
     plotallan_phase(plt, phaseC, 1, t, 'bo', 'true C phase')
     print("phaseC")
     
+    # clock differences
     plotallan_phase(plt, phaseAB, 1, t, 'r.', 'AB measurement')    
     print("phaseAB")
     plotallan_phase(plt, phaseBC, 1, t, 'g.', 'BC measurement')
@@ -76,7 +82,20 @@ if __name__ == "__main__":
     plotallan_phase(plt, phaseCA, 1, t, 'b.', 'CA measurement')
     print("phaseCA")
     
+    # 3CH estimates
+    (taus,devA,err_a,ns_ab) = allantools.three_cornered_hat_phase(phaseAB,phaseBC,phaseCA,rate,t, allantools.oadev)
     plt.loglog(taus, devA, 'rv', label='3-C-H estimate for A')
+    
+    (taus,devB,err_b,ns_ab) = allantools.three_cornered_hat_phase(phaseBC,phaseCA,phaseAB,rate,t, allantools.oadev)
+    plt.loglog(taus, devB, 'gv', label='3-C-H estimate for B')
+    
+    (taus,devC,err_C,ns_ac) = allantools.three_cornered_hat_phase(phaseCA,phaseAB,phaseBC,rate,t, allantools.oadev)
+    plt.loglog(taus, devC, 'bv', label='3-C-H estimate for C')
+    
+    print("TCH done.")
+    plt.title('AllanTools three-cornered-hat example')
+    plt.xlabel('Tau / s')
+    plt.ylabel('OADEV')
     plt.legend()
     plt.grid()
     plt.show()
