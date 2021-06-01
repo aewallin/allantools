287aa6fd2ea08af04598c7fabbc4854c0ae40819
Anders Wallin <anders.e.e.wallin@gmail.com>
2021-06-01 21:28:03 +0300

    pu3 print

--- examples/noise-color_and_PSD.py
+++ examples/noise-color_and_PSD.py
@@ -8,8 +8,8 @@ def many_psds(k=2,fs=1.0, b0=1.0, N=1024):
     """ compute average of many PSDs """
     psd=[]
     for j in range(k):
-        print j
-        x = noise.white(N=2*4096,b0=b0,fs=fs)
+        print(j)
+        x = noise.white(num_points=2*4096,b0=b0,fs=fs)
         f, tmp = noise.numpy_psd(x,fs)
         if j==0:
             psd = tmp
@@ -21,14 +21,14 @@ def many_psds(k=2,fs=1.0, b0=1.0, N=1024):
 fs=256 # sample rate
 b0=0.0002
 N=2*4096
-x = noise.white(N=2*4096,b0=b0,fs=fs)
+x = noise.white(num_points=2*4096,b0=b0,fs=fs)
 t = np.linspace(0, (1.0/fs)*N, len(x))
 
 plt.figure()
 plt.plot(t,x)
 plt.xlabel('Time / s')
 plt.ylabel('Amplitude / V')
-print x
+print(x)
 f, psd = many_psds(k=50,fs=fs,b0=b0,N=N)
 
 fxx, Pxx_den = noise.scipy_psd(x, fs)
