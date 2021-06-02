a3b8a7e5e2721f3937e26a59fd69bed6892cc292
Anders Wallin <anders.e.e.wallin@gmail.com>
2021-06-02 18:57:36 +0300

    no submodule

--- tests/functional_tests/test_mask.py
+++ tests/functional_tests/test_mask.py
@@ -1,5 +1,5 @@
 # from allantools import mask
-import allantools.mask as mask
+import allantools
 import numpy as np
 import pytest  # noqa
 
@@ -11,48 +11,48 @@ sys.path.append("..")
 
 
 def test_prc_tdev():
-    assert np.isclose(mask.prc_tdev(50),  3e-9)
-    assert np.isclose(mask.prc_tdev(500),  15e-9)
-    assert np.isclose(mask.prc_tdev(5000),  30e-9)
+    assert np.isclose(allantools.mask.prc_tdev(50),  3e-9)
+    assert np.isclose(allantools.mask.prc_tdev(500),  15e-9)
+    assert np.isclose(allantools.mask.prc_tdev(5000),  30e-9)
 
 
 def test_prc_mtie():
-    assert np.isclose(mask.prc_mtie(50),  0.03875e-6)
-    assert np.isclose(mask.prc_mtie(5000),  0.34e-6)
+    assert np.isclose(allantools.mask.prc_mtie(50),  0.03875e-6)
+    assert np.isclose(allantools.mask.prc_mtie(5000),  0.34e-6)
 
 
 def test_eprtc_tdev():
-    assert np.isclose(mask.eprtc_tdev(5), 1e-9)
-    assert np.isclose(mask.eprtc_tdev(50), .001666665e-9)
+    assert np.isclose(allantools.mask.eprtc_tdev(5), 1e-9)
+    assert np.isclose(allantools.mask.eprtc_tdev(50), .001666665e-9)
 
 
 def test_eprtc_mtie():
-    assert np.isclose(mask.eprtc_mtie(.5), 4e-9)
-    assert np.isclose(mask.eprtc_mtie(5), 4.4457e-9)
-    assert np.isclose(mask.eprtc_mtie(250), 15.009375e-9)
-    assert np.isclose(mask.eprtc_mtie(500000), 30e-9)
+    assert np.isclose(allantools.mask.eprtc_mtie(.5), 4e-9)
+    assert np.isclose(allantools.mask.eprtc_mtie(5), 4.4457e-9)
+    assert np.isclose(allantools.mask.eprtc_mtie(250), 15.009375e-9)
+    assert np.isclose(allantools.mask.eprtc_mtie(500000), 30e-9)
 
 
 def test_prtcA_tdev():
-    assert np.isclose(mask.prtcA_tdev(5), 3e-9)
-    assert np.isclose(mask.prtcA_tdev(500), 15e-9)
-    assert np.isclose(mask.prtcA_tdev(5000), 30e-9)
+    assert np.isclose(allantools.mask.prtcA_tdev(5), 3e-9)
+    assert np.isclose(allantools.mask.prtcA_tdev(500), 15e-9)
+    assert np.isclose(allantools.mask.prtcA_tdev(5000), 30e-9)
 
 
 def test_prtcB_tdev():
-    assert np.isclose(mask.prtcB_tdev(5), 1e-9)
-    assert np.isclose(mask.prtcB_tdev(250), 2.5e-9)
-    assert np.isclose(mask.prtcB_tdev(5000), 5e-9)
+    assert np.isclose(allantools.mask.prtcB_tdev(5), 1e-9)
+    assert np.isclose(allantools.mask.prtcB_tdev(250), 2.5e-9)
+    assert np.isclose(allantools.mask.prtcB_tdev(5000), 5e-9)
 
 
 def test_prtcA_mtie():
-    assert np.isclose(mask.prtcA_mtie(5), 0.026375e-6)
-    assert np.isclose(mask.prtcA_mtie(500), .1e-6)
+    assert np.isclose(allantools.mask.prtcA_mtie(5), 0.026375e-6)
+    assert np.isclose(allantools.mask.prtcA_mtie(500), .1e-6)
 
 
 def test_prtcB_mtie():
-    assert np.isclose(mask.prtcB_mtie(5), 0.026375e-6)
-    assert np.isclose(mask.prtcB_mtie(500), .04e-6)
+    assert np.isclose(allantools.mask.prtcB_mtie(5), 0.026375e-6)
+    assert np.isclose(allantools.mask.prtcB_mtie(500), .04e-6)
 
 
 if __name__ == "__main__":
