866efd05c9efcd8cda3fe51fb158df4edacfec51
Anders Wallin <anders.e.e.wallin@gmail.com>
2023-11-26 19:57:06 +0200

    coverage workflow

--- coverage.sh
+++ coverage.sh
@@ -4,6 +4,6 @@
 # works at least on Ubuntu 16.04LTS
 # AW2016-11-13
 
-python-coverage run --source allantools setup.py test
-python-coverage html
-python-coverage report
+python3-coverage run --source allantools setup.py test
+python3-coverage html
+python3-coverage report
