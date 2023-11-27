1be2c2e2f9a5ed74975bd914787a92ac76df6edb
Anders Wallin <anders.e.e.wallin@gmail.com>
2023-11-27 20:47:56 +0200

    napoleon ext

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ author = 'Anders E. Wallin'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
                 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
