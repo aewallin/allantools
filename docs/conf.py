56b10542542f8751dee96d21fa7c651797e36a14
Anders Wallin <anders.e.e.wallin@gmail.com>
2023-11-27 19:26:02 +0200

    sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@ author = 'Anders E. Wallin'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+                'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +25,7 @@ root_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+#html_theme = 'classic'
+# https://sphinx-rtd-theme.readthedocs.io/en/stable/installing.html
+html_theme = "sphinx_rtd_theme"
 # html_static_path = ['_static']
