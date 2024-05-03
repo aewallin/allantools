6abbcb3d39914c7258fb7a6b82a4b64a2268a50c
Montare, Aidan A. (Fed) <aidan.montare@nist.gov>
2024-05-03 16:25:17 -0600

    fix pep8

--- tests/test_package__all__.py
+++ tests/test_package__all__.py
@@ -1,7 +1,8 @@
 """
 Test that `from allantools import *` works.
 """
-from allantools import * # must be top-level, not allowed in function
+from allantools import *  # must be top-level, not allowed in function
+
 
 def test_package():
     pass
