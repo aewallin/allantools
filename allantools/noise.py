7d1558b7ef0fa93380355fe460de5748695026f9
Anders E.E. Wallin <anders.e.e.wallin@gmail.com>
2026-05-09 08:17:40 +0300

    escape

--- allantools/noise.py
+++ allantools/noise.py
@@ -217,12 +217,12 @@ def timmer_koenig_from_psd(f_nodes, h, alpha, duration, timestep, output='phase'
     
     .. math::
         
-        \begin{aligned}
+        \\begin{aligned}
         n  &= \\operatorname{int}(duration/dt) \\\
         f_1 &= 1 / ((n - 1) dt) \\\
         f_n &= 1 / (2  dt) \\\
         f_k &= \\operatorname{linspace}(f_1, f_n, n/2 + 1)
-        \end{aligned}
+        \\end{aligned}
 
     Sample one-sided fractional-frequency PSD:
         :math:`S_y(f) = h_i f^{\\alpha_i}`
